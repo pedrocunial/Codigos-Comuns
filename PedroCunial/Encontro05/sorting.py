b049eb43e325318e39831630dd1db78dba5ae60b
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-04 19:09:58 -0300

    quick commit

---
+++ PedroCunial/Encontro05/sorting.py
@@ -0,0 +1,11 @@
+with open("A-large-practice.in", "r") as f, open("output.out", "w") as out:
+    T = int(f.readline().strip())
+    for caso in range(T):
+        n = int(f.readline().strip())
+        print(n)
+        v1 = [int(x) for x in f.readline().strip().split()]
+        v1.sort()
+        v2 = [int(x) for x in f.readline().strip().split()]
+        v2.sort(reverse=True)
+        soma = sum([x*y for x, y in zip(v1, v2)])
+        out.write("Case #{}: {}\n".format(caso + 1, soma))
